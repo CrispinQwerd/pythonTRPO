48deaf697441f5913c97ce7568ee17c623ba101d
CrispinQwerd <150508517+CrispinQwerd@users.noreply.github.com>
2024-03-02 10:02:27 +0300

    lab2

---
+++ lab02/TRPO2.2.py
@@ -0,0 +1,5 @@
+var1 = int(input("Введите число "))
+var2 = int(input("Введите второе число "))
+summ = var1 + var2
+print(summ)
+1
---
+++ lab02/TRPO2.py
@@ -0,0 +1,6 @@
+var1 = 10
+var2 = 15
+summ = var1 + var2
+print(summ)
+
+

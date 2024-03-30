27a1dacded2bbc6e627918923fe0fd924ea65301
CrispinQwerd <nord123play@gmail.com>
2024-03-30 09:37:01 +0300

    interactivemenu

--- lab08/lab8.py
+++ lab08/lab8.py
@@ -1,39 +1,61 @@
 import random
+
+def get_user_choice():
+    while True:
+        print("Выберите действие:")
+        print("1. Камень")
+        print("2. Ножницы")
+        print("3. Бумага")
+        choice = input("Введите число от 1 до 3: ")
+        if choice in ["1", "2", "3"]:
+            return choice
+        else:
+            print("Неправильный ввод. Попробуйте снова.")
+
+def get_bot_choice():
+    return random.choice(["1", "2", "3"])
+
+def get_choice_name(choice):
+    if choice == "1":
+        return "камень"
+    elif choice == "2":
+        return "ножницы"
+    elif choice == "3":
+        return "бумага"
+
+def determine_winner(player_choice, bot_choice):
+    if player_choice == bot_choice:
+        return "Ничья!"
+    elif (player_choice == "1" and bot_choice == "2") or \
+         (player_choice == "2" and bot_choice == "3") or \
+         (player_choice == "3" and bot_choice == "1"):
+        return "Ты победил!"
+    else:
+        return "Я победил!"
+
 print("Добро пожаловать в игру камень ножницы бумага!")
-playerScore = 0
-botScore = 0
+player_score = 0
+bot_score = 0
 
-for i in range(3):
-    answer = input("Что выберешь?\n").lower()
+for _ in range(3):
+    player_choice = get_user_choice()
+    bot_choice = get_bot_choice()
 
-    if "камень" in answer:
-        answer = "к"
-    elif "ножницы" in answer:
-        answer = "н"
-    elif "бумага" in answer:
-        answer = "б"
+    print(f"Ты выбрал {get_choice_name(player_choice)}, а я выберу {get_choice_name(bot_choice)}")
+
+    result = determine_winner(player_choice, bot_choice)
+    print(result)
+
+    if "победил" in result:
+        player_score += 1
+    elif "Ничья" in result:
+        pass
     else:
-        print("Неправильный ввод. Попробуй еще раз.")
-        continue
+        bot_score += 1
 
-    botAnswer = random.choice(["камень", "ножницы", "бумага"])
-    print(f"А я выберу {botAnswer}")
-    botAnswer = botAnswer[0]
-
-    if answer == botAnswer:
-        print("Ничья!")
-    elif (answer == "к" and botAnswer == "н") or \
-         (answer == "н" and botAnswer == "б") or \
-         (answer == "б" and botAnswer == "к"):
-        print("Ты победил!")
-        playerScore += 1
-    else:
-        print("Я победил!")
-        botScore += 1
-
-if playerScore == botScore:
+if player_score == bot_score:
     print("Ничья по итогам трех раундов!")
-elif playerScore > botScore:
+elif player_score > bot_score:
     print("Ты победил по итогам трех раундов!")
 else:
     print("Я победил по итогам трех раундов!")

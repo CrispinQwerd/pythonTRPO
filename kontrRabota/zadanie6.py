cbb00a6b3c5471eaa30ecc84a561b1fdf46d351f
CrispnQwerd <nord123play@gmail.com>
2024-04-13 10:11:25 +0300

    add zadanie6

---
+++ kontrRabota/zadanie6.py
@@ -0,0 +1,22 @@
+import turtle 
+import random 
+
+t = turtle.Turtle()
+t.pensize(3)
+turtle.speed(200) 
+t.shape("turtle")
+r=int(input("Введите радиус первого круга: ->  ")) 
+m = 4
+colors=["blue","orange","green","red"] 
+
+
+for i in range(m+1): 
+    color = colors[i%4]
+    t.circle(r * i) 
+    t.up() 
+    t.sety((r * i)*(-1)) 
+    t.down() 
+    t.pencolor(color)
+
+
+input("Press the Enter key to continue: ")
